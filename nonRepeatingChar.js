650ef637734dd837a637c0691961809efe7c1fca
ssurajita <ssurajita111@gmail.com>
2019-01-06 20:21:50 +0530

    nonRepeatingChar.js

---
+++ nonRepeatingChar.js
@@ -0,0 +1,21 @@
+/** Given a string, find its first non-repeating character **/
+
+var str = geeksforgeeks;
+var obj = {};
+var arr = str.split("");
+function firstNonRepeatingChar(str){
+  count =1;
+  for(var i=0; i< arr.length; i++){
+    if(obj.hasOwnProperty(arr[i])){
+      obj[arr[i]] = count + 1;
+    }else{
+      obj[arr[i]] = count;
+    }
+  }
+
+ for (let key in obj){
+  if(obj[key] == 1){
+    return key;
+  }
+ }
+}
