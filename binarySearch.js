2641f763787d2689b8e50a21a8d8e66d5fe8b207
ssurajita <ssurajita111@gmail.com>
2019-01-06 19:51:46 +0530

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -1,1 +1,14 @@
 
+function binarySearch(arr,target){
+  let left = 0;
+  let right = arr.length - 1;
+  while(left < right){
+    let mid = left + Math.floor((right-left)/2);
+    if(arr[mid] === target){ return mid; }
+    if(arr[mid]<target){ left = mid+1; }
+    if(arr[mid]>target){ right = mid-1; }
+  }
+  return -1;
+}
+
+binarySearch([1,3,4,6,8,9,10,20],9);
