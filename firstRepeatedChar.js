5612753c4717f8c3a8171d53aee2d91faa922319
ssurajita <ssurajita111@gmail.com>
2019-01-06 20:25:07 +0530

    Update firstRepeatedChar.js

--- firstRepeatedChar.js
+++ firstRepeatedChar.js
@@ -1,1 +1,24 @@
 
+/** Given a string, find its first repeating character **/
+
+var str = geeksforeeks;
+var obj = {};
+var arr = str.split("");
+function firstRepeatingChar(str){
+  count =1;
+  for(var i=0; i< arr.length; i++){
+    if(obj.hasOwnProperty(arr[i])){
+      obj[arr[i]] = count + 1;
+    }else{
+      obj[arr[i]] = count;
+    }
+  }
+
+ for (let key in obj){
+  if(obj[key] > 1){
+    return key;
+  }
+ }
+}
+
+firstRepeatingChar(str);
