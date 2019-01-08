60eb9538f78f9c4411d1dcbd48555d35b29e16fe
ssurajita <ssurajita111@gmail.com>
2019-01-08 16:56:15 +0530

    Update mergeSort.js

--- mergeSort.js
+++ mergeSort.js
@@ -1,7 +1,32 @@
-function mergesort(){
-
+function mergesort(arr){
+  if(arr.length == 1){
+    return arr;
+  }
+  
+  const middle = Math.floor(arr.length /2);
+  const left = arr.slice(0,middle);
+  const right = arr.slice(middle);
+  
+  return merge(mergesort(left),mergesort(right));
 }
 
-function merge(){
-
+function merge(left,right){
+  result= [];
+  leftIndex=0;
+  rightIndex=0;
+  
+  while(leftIndex < left.length && rightIndex < right.length){
+    if(left[leftIndex] < right[rightIndex]){
+      result.push(left[leftIndex]);
+      leftIndex++;
+    }else{
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex))
 }
+
+const list = [2, 5, 1, 3, 7, 2, 3, 8, 6, 3];
+console.log(mergesort(list)) // [ 1, 2, 2, 3, 3, 3, 5, 6, 7, 8 ]
