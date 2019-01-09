70ed168d189efc22f65c7e16d4add0d4d82e3b71
ssurajita <ssurajita111@gmail.com>
2019-01-09 14:59:02 +0530

    eggDropping.js

---
+++ eggDropping.js
@@ -0,0 +1,16 @@
+function eggDrop(e,f){
+  //e eggs , f floors
+  
+  if(f == 1 || f == 0){ return f;}
+  if(e==1){ return f; }
+  
+  var min = Infinity;
+  var x,res;
+  
+  for(x=1;x<=f;x++){
+  
+  }
+}
+
+
+eggDrop(2,10);
