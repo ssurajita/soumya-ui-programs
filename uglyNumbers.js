93b7b40124d81bd288e94a8a63f06eaa83f385af
ssurajita <ssurajita111@gmail.com>
2019-01-10 12:31:24 +0530

    Update uglyNumbers.js

--- uglyNumbers.js
+++ uglyNumbers.js
@@ -15,9 +15,16 @@ function getNthUglyNo(n){
         if(nextUgly == nextMulti2){ u2++; nextMulti2  = ugly[u2]*2; }
         if(nextUgly == nextMulti3){ u3++; nextMulti3  = ugly[u3]*3; }
         if(nextUgly == nextMulti5){ u5++; nextMulti5  = ugly[u5]*5; }
+        console.log(u2,u3,u5); 
     }console.log(ugly);
     return nextUgly;
 }
  
  getNthUglyNo(7);//8
  getNthUglyNo(12);//16
+
+
+//i=1, nextUgly =2, firstIf ,u2=1,nextMulti2 =4
+//i=2, nextUgly =3, secondIf ,u3=1,nextMulti3 =6
+//i=3, nextUgly =4, firstIf ,u2=2,nextMulti2 =6
+
